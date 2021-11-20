11d98a548aa4bcdf4d1b0f09152db78018e683ce
NielsG-2054772 <niels.gielen@student.uhasselt.be>
2021-11-20 16:48:56 +0100

    edited website

--- index.js
+++ index.js
@@ -1,22 +1,184 @@
 var element = document.getElementById("text");
-var zerohit = true;
+var zerohitl = true;
+var zerohitt = true;
+var red = false;
+const colors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "RebeccaPurple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "SlateGrey",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen",
+];
+function swapBackground(){
+  document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  while(document.body.style.backgroundColor == element.style.backgroundColor){
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  }
+}
+
+function swapColor() {
+  const random = Math.floor(Math.random() * colors.length);
+  element.style.backgroundColor = colors[random];
+  swapBackground();
+}
+
 function move() {
   var rect = element.getBoundingClientRect();
   var left = rect.left;
+  var top = rect.top;
   console.log(window.innerWidth);
   if (left <= 0) {
-    zerohit = true;
+    zerohitl = true;
+    swapColor();
   }
-  if (left + 260 >= window.innerWidth) {
-    zerohit = false;
+  if (left + 100 >= window.innerWidth) {
+    zerohitl = false;
+    swapColor();
   }
-  if (zerohit) {
-    left += 10;
+  if (zerohitl) {
+    left += 2;
   } else {
-    left -= 10;
+    left -= 2;
   }
-  console.log("left: " + left);
+  if (top >= window.innerHeight - 110) {
+    zerohitt = true;
+    swapColor();
+  }
+  if (top <= 0) {
+    zerohitt = false;
+    swapColor();
+  }
+  if (zerohitt) {
+    top -= 2;
+  } else {
+    top += 2;
+  }
   element.style.left = left + "px";
+  element.style.top = top + "px";
 }
 
-setInterval(move, 100);
+setInterval(move, 10);
